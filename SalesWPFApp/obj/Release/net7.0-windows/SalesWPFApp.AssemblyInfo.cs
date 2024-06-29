85a33d08c772e89a9e9f4f53e9554f7693d8cc0a
kanatsumi <ducnase171103@fpt.edu.vn>
2024-06-29 17:57:45 +0700

    ignore bin obj

--- SalesWPFApp/obj/Release/net7.0-windows/SalesWPFApp.AssemblyInfo.cs
+++ SalesWPFApp/obj/Release/net7.0-windows/SalesWPFApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWPFApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad6fca006aec5b299af0422f54290e8722fb6bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWPFApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWPFApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
